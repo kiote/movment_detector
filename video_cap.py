8c4fc1a70f673debcd1f2e07a73f1ae799919d69
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2019-02-09 10:59:00 +0200

    add video cap

---
+++ video_cap.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    fgmask = fgbg.apply(gray)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+ 
+
+    # Display the resulting frame
+    cv2.imshow('frame',fgmask)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
