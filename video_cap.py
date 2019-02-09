5a8720cf06b09f2c9e8215729b8f50036d14b07d
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2019-02-09 11:30:01 +0200

    movement detection based on pictures diff

--- video_cap.py
+++ video_cap.py
@@ -1,26 +1,36 @@
 import numpy as np
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
 
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
+ret, frame1 = cap.read()
 
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, frame2 = cap.read()
+
+    # Create an image based on the differences between the two frames and then enhance the result
+    diffImg = cv2.absdiff(frame1, frame2)
+    # threshImg = cv2.threshold(diffImg, PIXEL_INTENSITY_THRESHOLD, 255, cv2.THRESH_BINARY)[1]
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(diffImg, cv2.COLOR_BGR2GRAY)
     fgmask = fgbg.apply(gray)
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
- 
 
+	# Assign frame 1 to frame 2 for the next iteration of comparison
+    frame1 = frame2
+ 
     # Display the resulting frame
     cv2.imshow('frame', fgmask)
 
     whitePixels = cv2.findNonZero(fgmask)
+
+    time.sleep(0.1)
 
     if whitePixels is not None:
         amountOfMovement = len(whitePixels)
